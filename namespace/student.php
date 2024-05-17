f277d89039b04a8f497e2720d9d8fe37bddf7645
Piyush Dubey <89024653+EngineerPiyush@users.noreply.github.com>
2024-05-17 12:49:29 +0530

    Add files via upload

---
+++ namespace/student.php
@@ -0,0 +1,16 @@
+<?php 
+// namespace student;
+// class JoiningDetails{
+//     function admissionDate(){
+//         echo "01/05/2019";
+//     }
+// }
+// or wen can declare namespace like below mwthod also
+namespace student{
+class JoiningDetails{
+    function admissionDate(){
+        echo "01/05/2019";
+    }
+}
+}
+?>
